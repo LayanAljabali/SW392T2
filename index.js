375c583a51d8f58070ebb4fc340b9b927a081ae1
LayanAljabali <93729711+LayanAljabali@users.noreply.github.com>
2021-12-15 21:51:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,52 +4,51 @@ var FastText = require('node-fasttext');
 const cors = require('cors');
 
 let config = { 
-  dim: 100,
-  input: "train.txt",
-  output: "model"
+ dim: 100,
+ input: "train.txt",
+ output: "model"
 }
-
 
 FastText.train("supervised", config, function (success, error) {
 
-  if(error) {
-    console.log(error)
-    return;
-  }
-  
-  console.log(success)
-  
+ if(error) {
+   console.log(error)
+   return;
+ }
+
+ console.log(success)
+
 })
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.sendfile("index.html");
+ res.sendfile("index.html");
 });
 
 app.get('/fasttext/', function(req, res) {
-  var statement = req.param('statement');
-    res.send(getFastTextResults(statement));
+ var statement = req.param('statement');
+   res.send(getFastTextResults(statement));
 });
 
 function getFastTextResults(statement) {
-	//predict returns an array with the input and predictions for best cateogires
-	FastText.predict(
-		"model.bin", 3,
-		[statement],
-		function (success, error) {
+   //predict returns an array with the input and predictions for best cateogires
+   FastText.predict(
+       "model.bin", 3,
+       [statement],
+       function (success, error) {
 
-		  if(error) {
-			console.log(error)
-			return;
-		  }
-		  s=success
-		  console.log(success)
-		})
-	return s;
+         if(error) {
+           console.log(error)
+           return;
+         }
+         s=success
+         console.log(success)
+       })
+   return s;
 }
 
 app.listen(8000, () => {
-  console.log('Listening on port 8000!')
+ console.log('Listening on port 8000!')
 });
 
